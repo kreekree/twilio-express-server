1d80c66a2c98aa9eebfa2c19aedd141250a4fd49
Shukree Abuwi <shushuhead@gmail.com>
2023-06-04 20:28:25 -0400

    fix 2

--- api/index.js
+++ api/index.js
@@ -15,7 +15,4 @@ app.post('/answer', (req, res) => {
   res.send(twiml.toString())
 })
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+module.exports = app
