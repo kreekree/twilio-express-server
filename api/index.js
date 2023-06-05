9ffaf6b620589b0d3b0b3914e1ae94e99ecd7749
Shukree Abuwi <shushuhead@gmail.com>
2023-06-04 21:50:58 -0400

    twilio

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 const app = require('express')()
 const { v4 } = require('uuid')
-
-app.get('/api/cool', (req, res) => {
-  res.send('Hello, world!')
-})
+const twilio = require('twilio')
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+})
+
+app.post('/api/answer', (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse()
+
+  twiml.say('Hello! Welcome to NoGigiddy. How can I help you?')
+
+  res.type('text/xml')
+  res.send(twiml.toString())
 })
 
 app.get('/api/item/:slug', (req, res) => {
